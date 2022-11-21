211c90332360982e40e1f8c41e3e61e6978c9987
BA\chanhnv <vanchanhnguyen95@gmail.com>
2022-11-21 09:55:41 +0700

    add src SwaggerService

---
+++ SwaggerService/Controllers/RouteSpeedProviderController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using SwaggerService.Models;
+
+namespace SwaggerService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RouteSpeedProviderController : ControllerBase
+    {
+        //GET: https://localhost:44352/api/RouteSpeedProvider?limit=100
+        [HttpGet(Name = "RouteSpeedProvider/Get")]
+        public List<SpeedLimit> Get(int? limit)
+        {
+            if (limit != null)
+            {
+                return GetSpeedProviders();
+            }
+            return GetSpeedProviders();
+        }
+
+        [HttpPost(Name = "Push")]
+        public SpeedLimit Push([FromBody] SpeedLimit speedLimit)
+        {
+            return new SpeedLimit();
+        }
+
+        private List<SpeedLimit> GetSpeedProviders()
+        {
+            return new List<SpeedLimit>()
+            {
+                new SpeedLimit()
+                {
+                    Id = 1,
+                    Long = 1,
+                    Lat = 1,
+                    MinSpeed = 0,
+                    MaxSpeed = 50,
+                    ProviderType = 1
+                },
+                new SpeedLimit()
+                {
+                    Id = 2,
+                    Long = 1,
+                    Lat = 1,
+                    MinSpeed = 0,
+                    MaxSpeed = 50,
+                    ProviderType = 1
+                },
+                new SpeedLimit()
+                {
+                    Id = 3,
+                    Long = 1,
+                    Lat = 1,
+                    MinSpeed = 0,
+                    MaxSpeed = 50,
+                    ProviderType = 1
+                },
+                new SpeedLimit()
+                {
+                    Id = 4,
+                    Long = 1,
+                    Lat = 1,
+                    MinSpeed = 0,
+                    MaxSpeed = 50,
+                    ProviderType = 1
+                },
+            };
+        }
+
+    }
+}
---
+++ SwaggerService/Models/SpeedLimit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwaggerService.Models
+{
+    public class SpeedLimit
+    {
+        public int Id { get; set; }
+        public double Lat { get; set; } = 0;
+        public double Long { get; set; } = 0;
+        public int MinSpeed { get; set; } = 0;
+        public int MaxSpeed { get; set; } = 0;
+        public int ProviderType { get; set; } = 1;
+    }
+}
